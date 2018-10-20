ad2e184f2955c8fdc948d6460661616d3b6342ed
nakyla <pennyangel90@gmail.com>
2018-10-20 18:29:58 +0000

    add if else

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #   end
   get "add/:x/:y" => "application#add"
   get "todo/index" , to: "todo#index"
-  get "todo/show" , to: "todo#show"
+  get "todo/show/:id" , to: "todo#show"
 end
