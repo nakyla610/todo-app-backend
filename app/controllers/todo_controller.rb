55f78ffb56c8b340458803f7769b76772b5b11c0
nakyla <pennyangel90@gmail.com>
2018-10-21 16:41:04 +0000

    todo form

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,5 +4,16 @@ class TodoController < ApplicationController
     end
     def show
         @todo = Todo.find_by_id(params[:id])
-        end
     end
+    def new
+        @todo = Todo.new
+    end
+    def create
+        t= Todo.new
+        t.description = params["description"]
+        t.pomodoro_estimate = params["pomodoro_estimate"]
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,6 @@ Rails.application.routes.draw do
   get "add/:x/:y" => "application#add"
   get "todo/index" , to: "todo#index"
   get "todo/show/:id" , to: "todo#show"
+  get "todo/new" , to: "todo#new"
+  get "todo/create" , to: "todo#create"
 end
